47e0316b6679b378a4c00c00d292939ad340e3a1
Venugopalkrishna <s_venugopalkrishna@tss-hr420-dm13.fc.ca>
2018-02-09 11:29:39 -0500

    EJS

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const templateTypes = [
+  {
+    name: "Jade",
+    usefulness: "sucky"
+  },
+  {
+    name: "EJS",
+    usefulness: "better"
+  },
+  {
+    name: "Handlebars",
+    usefulness: "awesome"
+  }
+]
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', description : "sucks!" , method: "easy!"});
+  res.render('index', { title: 'EJS', description : "easy!" , method: "k!", templates: templateTypes});
 });
 
 module.exports = router;
